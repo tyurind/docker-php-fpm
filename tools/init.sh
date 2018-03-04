f2a80b82d8f5ca07a332909d3cdc5cf1a3e8a869
Dmitriy Tyurin <fobia3d@gmail.com>
2018-03-04 06:44:59 +0300

    _compose

--- tools/init.sh
+++ tools/init.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-PUID=$(id -u)
-PGID=$(id -g)
+D_UID=$(id -u)
+D_GID=$(id -g)
 
-env | grep SUDO
+echo "D_UID: $D_UID"
+echo "D_GID: $D_GID"
+echo "SUDO_UID: $SUDO_UID"
+echo "SUDO_GID: $SUDO_GID"
 
+# PUID=${PUID-33}
+# PGID=${PGID-33}
+
+# env | grep SUDO
+#
 
 # SUDO_GID=1000
 # SUDO_COMMAND=/bin/bash init.sh
 # SUDO_USER=tyurin
 # SUDO_UID=1000
+
+# I_UID=${SUDO_UID-$D_UID}
+# I_GID=${SUDO_GID-$D_GID}
+
+I_UID=${SUDO_UID-`id -u`}
+I_GID=${SUDO_GID-`id -g`}
+
+USERWORK="${I_UID}:${I_GID}"
+
+echo $USERWORK
 
