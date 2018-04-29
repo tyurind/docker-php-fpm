2ce821fcde3536101eebd294983c8496103daa18
Dmitriy Tyurin <fobia3d@gmail.com>
2018-04-29 13:00:37 +0300

    ---

--- tools/lib/init-go-work.sh
+++ tools/lib/init-go-work.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 FILE_LOCK=/etc/init-worker.lock
+FILE_WORKNAME=/etc/workuser
 
 # SUDO_UID=1000
 
@@ -22,12 +23,17 @@ su_useradd()
     chmod -R g+w "${user_home}"
 }
 
-su_groupmod()
+
+su_install()
 {
-    groupadd -f --system dev 2>/dev/null
+    local user_name="$1"
+    if [ "x${user_name}" != "x" ]; then
+        echo "${user_name}" | tee "${FILE_WORKNAME}" > /dev/null
+    fi
+
+    groupadd -f --system dev
     echo "%dev   ALL=(root) NOPASSWD: /sbin/init-go-work.sh" >> /etc/sudoers
 }
-
 
 # usermod id
 #
@@ -49,7 +55,8 @@ su_usermod()
 
     if [ "${old_uid}" != "${uid}" ]; then
         # echo "usermod -u $uid -o ${user_name} 2> /dev/null"
-        usermod -u "$uid" -o "${user_name}" 2> /dev/null
+        usermod -u "$uid" -o "${user_name}"
+        usermod -a -G dev "${user_name}"
 
         user_home=$(sed -n "/^${user_name}:/p" /etc/passwd | cut --delimiter=":" -f6 | sed -n '/^\/home\//p')
         if [ "x${user_home}" != "x" ]; then
@@ -59,17 +66,21 @@ su_usermod()
     fi
 }
 
-
 __init_work()
 {
-    if [ "x${WORKNAME}" != "x" -a ! -f "${FILE_LOCK}" ]; then
+    local workuser="${WORKNAME}"
+    if [ "x${workuser}" = "x" -a -f "${FILE_WORKNAME}" ]; then
+        workuser=$( cat "${FILE_WORKNAME}" )
+    fi
+
+    if [ "x${workuser}" != "x" -a ! -f "${FILE_LOCK}" ]; then
         if [ "$(id -u)" != "0" ]; then
             local dir_source=$( cd $( dirname "$0" ) && pwd )
             local sh_source=$( basename "$0" )
 
-            # echo "sudo sh ${dir_source}/${sh_source} -i ${WORKNAME}"
+            # echo "sudo sh ${dir_source}/${sh_source} -i ${workuser}"
 
-            sudo sh "${dir_source}/${sh_source}" -i "${WORKNAME}"
+            sudo sh "${dir_source}/${sh_source}" -i "${workuser}"
             return 1
         fi
 
@@ -77,7 +88,7 @@ __init_work()
         uid=$(stat -c '%u' $stat_dir)
 
 
-        user_name=$( id -u "${WORKNAME}" 2>/dev/null )
+        user_name=$( id -u -n "${workuser}" 2>/dev/null )
         if [ "x${user_name}" = "x" ]; then
             su_useradd "${user_name}" "/home/${user_name}"
         fi
@@ -91,16 +102,54 @@ __init_work()
     fi
 }
 
+
+## Example:
+##     $ sudo init-go-work.sh  --install www-data
+if [ $# -eq "1" -a "${1}" = "--help" ] ; then
+    cat <<EOF
+
+Usage:
+
+    Инициализирует пользовательскую группу
+    $ sudo ${0} --install
+
+    Инициализирует пользовательскую группу и установить пользователя по умолчанию
+    $ sudo ${0} --install www-data
+
+
+    $ sudo ${0} -i www-data
+
+    Установит права пользователю WORKUSER, если они еще не установлены, перед выполнением команды
+    $ ${0} <command> [args...]
+
+EOF
+    exit 0
+fi
+
+
+
+## Example:
+##     $ sudo init-go-work.sh  --install www-data
+if [ $# -ge "1" -a "${1}" = "--install" ] ; then
+    su_install "$2"
+    exit $?
+fi
+
+
+## Example:
+##     $ sudo init-go-work.sh -i www-data
 if [ $# -eq "2" -a "${1}" = "-i" -a "x${2}" != "x" ] ; then
     # echo "sudo $1 $2"
     WORKNAME="$2"
     __init_work
-    exit
+    exit $?
 else
     __init_work
 fi
 # =======================
 
+## Example:
+##     $ init-go-work.sh php-fpm
 if [ $# -ge "1" ] ; then
     exec "$@"
 fi
