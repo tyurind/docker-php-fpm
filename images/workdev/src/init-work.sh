0dffc68e7723d9549ff8dd8f8a66fbfa15791915
Dmitriy Tyurin <fobia3d@gmail.com>
2018-05-05 14:11:31 +0000

    init work user

---
+++ images/workdev/src/init-work.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+FILE_LOCK=/etc/workuser.lock
+WORKUSER=workuser
+
+__init_work()
+{
+    local user_name="${WORKUSER}"
+    #local    user_name=$( id -u -n "${workuser}" 2>/dev/null )
+
+    local stat_dir=$(pwd)
+    local uid=$(stat -c '%u' $stat_dir)
+    local old_uid=$(id -u "${user_name}")
+    local user_home=""
+
+
+    if [ "${old_uid}" != "${uid}" ]; then
+        # echo "usermod -u $uid -o ${user_name} 2> /dev/null"
+        usermod -u "$uid" -o "${user_name}"
+
+        user_home=$(sed -n "/^${user_name}:/p" /etc/passwd | cut --delimiter=":" -f6 | sed -n '/^\/home\//p')
+        if [ "x${user_home}" != "x" ]; then
+            chown -R "$uid" "$user_home"
+        fi
+    fi
+
+    id -u "${user_name}" | tee "${FILE_LOCK}" > /dev/null
+}
+
+if [ ! -f "${FILE_LOCK}" ]; then
+    __init_work
+fi
+
+exit 0
